dfcdef5c9ddf2467a7269300738fc018cf65341d
Trinh Minh Cuong <cuong@techmaster.vn>
2018-01-10 23:59:09 +0700

    Fix error2

--- fish.sh
+++ fish.sh
@@ -10,10 +10,12 @@ apt-add-repository -y ppa:fish-shell/release-2
 apt update
 apt install -y git fish
 
-curl -L https://get.oh-my.fish > ~/omf.fish \
-&& fish ~/omf.fish --noninteractive --path=~/.local/share/omf --config=~/.config/omf \
-&& chown -R $SUDO_USER /home/$SUDO_USER/.config/fish \
-&& chmod -R 755 /home/$SUDO_USER/.config/fish \
-&& echo 'omf install budspencer;fish_update_completions;set -U budspencer_nogreeting;rm ~/omf.fish;rm ~/.config/fish/config.fish' > ~/.config/fish/config.fish \
-&& chsh -s `which fish` $SUDO_USER \
-&& cp fishfunctions/*.fish /home/$SUDO_USER/.config/fish/functions/
+curl -L https://get.oh-my.fish > ~/omf.fish
+fish ~/omf.fish --noninteractive --path=~/.local/share/omf --config=~/.config/omf
+chown -R $SUDO_USER /home/$SUDO_USER/.config/fish
+chmod -R 755 /home/$SUDO_USER/.config/fish
+
+echo 'omf install budspencer;fish_update_completions;set -U budspencer_nogreeting;rm ~/omf.fish;rm ~/.config/fish/config.fish' > ~/.config/fish/config.fish
+
+chsh -s `which fish` $SUDO_USER
+cp fishfunctions/*.fish /home/$SUDO_USER/.config/fish/functions/
--- golang.sh
+++ golang.sh
@@ -7,4 +7,4 @@ tar -xvf go1.9.2.linux-amd64.tar.gz
 mv go /usr/local
 rm go1.9.2.linux-amd64.tar.gz
 touch ~/.config/fish/config.fish
-echo "set -gx PATH \$PATH /usr/local/go/bin" >> ~/.config/fish/config.fish
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
