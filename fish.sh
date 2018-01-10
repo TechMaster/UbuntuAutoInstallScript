d725fbc300e8a6df95e0ba1d40fe538f54ea6afb
Trinh Minh Cuong <cuong@techmaster.vn>
2018-01-10 17:27:40 +0700

    Add fish functions to alias most used commands

--- fish.sh
+++ fish.sh
@@ -15,5 +15,6 @@ curl -L https://get.oh-my.fish > ~/omf.fish \
 && chown -R $SUDO_USER /home/$SUDO_USER/.config/fish \
 && chmod -R 755 /home/$SUDO_USER/.config/fish \
 && echo 'omf install budspencer;fish_update_completions;set -U budspencer_nogreeting;rm ~/omf.fish;rm ~/.config/fish/config.fish' > ~/.config/fish/config.fish \
-&& chsh -s `which fish` \
+&& chsh -s `which fish` $SUDO_USER \
+&& cp fishfunctions/*.fish /Users/$SUDO_USER/.config/fish/functions \
 && fish
---
+++ fishfunctions/lc.fish
@@ -0,0 +1,4 @@
+function lc
+    clear
+    ls
+end
