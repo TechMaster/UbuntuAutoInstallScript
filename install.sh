6ebb64395f1daa12d53b2e6f673280ef1c4aa228
Trinh Minh Cuong <cuong@techmaster.vn>
2018-01-10 21:58:13 +0700

    Update script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-apt update \
-&& apt -y upgrade \
-&& apt install -y git \
-&& git clone https://github.com/TechMaster/UbuntuAutoInstallScript \
-&& cd UbuntuAutoInstallScript \
-&& ./basic.sh \
-&& ./docker.sh \
-&& ./fish.sh \
-&& ./vscodedotnet.sh \
-&& ./node.sh \
-&& ./golang.sh \
-&& ./sublime.sh \
-&& ./runFishShellAtLast.sh
+apt update
+apt -y upgrade
+apt -y install git
+git clone https://github.com/TechMaster/UbuntuAutoInstallScript
+cd UbuntuAutoInstallScript
+./basic.sh
+./docker.sh
+./fish.sh
+./vscodedotnet.sh
+./node.sh
+./golang.sh
+./sublime.sh
+./runFishShellAtLast.sh
